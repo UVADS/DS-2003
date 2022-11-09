65eff8f933ba92b0036e4e04e812f20001316f67
bacorliss <44813189+bacorliss@users.noreply.github.com>
2022-11-09 13:10:19 -0500

    Added code for assignment

---
+++ 12 Shiny/class 24/deployApp.R
@@ -0,0 +1,3 @@
+library(rsconnect)
+setwd("~/Library/CloudStorage/OneDrive-UniversityofVirginia/Courses/DS2003/12 Shiny/class 24/shinyApp")
+deployApp()
---
+++ 12 Shiny/class 24/runApp.R
@@ -0,0 +1,4 @@
+library(shiny)
+setwd("~/Library/CloudStorage/OneDrive-UniversityofVirginia/Courses/DS2003/12 Shiny/class 24/shinyApp")
+
+runApp()
